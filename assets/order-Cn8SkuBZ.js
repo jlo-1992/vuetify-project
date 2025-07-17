b1713426beb87e348634438462124d3cfef2e353
jlo-1992 <ts01545387@gmail.com>
2025-07-17 01:02:36 +0000

    Deploying to gh-pages from @ jlo-1992/vuetify-project@2c56bf844223ea711b92a14446111ac8d17388be 🚀

---
+++ assets/order-Cn8SkuBZ.js
@@ -0,0 +1,1 @@
+import{bK as r}from"./index-D5AbFdb1.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
